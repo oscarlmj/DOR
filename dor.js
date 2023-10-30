bc67e860b0aec739351547c017340b933c5d4350
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-10-30 13:50:07 +0000

    Subida

---
+++ dor.js
@@ -0,0 +1,10 @@
+function cambiarFondo()
+{
+    document.getElementById("parra").style.backgroundColor="gray";
+    document.getElementById("parra").style.transition="width 6s";
+}
+
+function mensaje()
+{
+    document.getElementById("columna 1").innerHTML="<dialog>Aloooooooo</dialog>"
+}
