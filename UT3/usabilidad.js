49e0b4337fe2b889dcc8d52ba4a0da2151bead0b
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-11-27 13:41:13 +0000

    Usa

--- UT3/usabilidad.js
+++ UT3/usabilidad.js
@@ -1,52 +1,25 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    const taskList = document.getElementById('taskList');
-    const taskForm = document.getElementById('taskForm');
-    const newTaskInput = document.getElementById('newTask');
+let lista=document.getElementById("lista");
+let formulario=document.getElementById("nuevaTarea");
+let tarea=document.getElementById("tarea");
+let fecha=document.getElementById("fecha");
 
-    taskForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        // Validar que el campo no esté vacío
-        if (newTaskInput.value.trim() === '') {
-            alert('Por favor, ingrese una tarea.');
-            return;
-        }
-
-        // Crear un nuevo elemento de lista (li) con la tarea
-        const newTaskElement = document.createElement('li');
-        newTaskElement.innerHTML = `
-            <span>${newTaskInput.value}</span>
-            <button onclick="editTask(this)">Editar</button>
-            <button onclick="deleteTask(this)">Eliminar</button>
-        `;
-
-        // Agregar el nuevo elemento de lista a la lista de tareas
-        taskList.appendChild(newTaskElement);
-
-        // Limpiar el campo de entrada después de agregar la tarea
-        newTaskInput.value = '';
-    });
-});
-
-function editTask(button) {
-    // Obtener el elemento de la tarea actual
-    const taskElement = button.parentNode;
-
-    // Obtener el texto actual de la tarea
-    const currentTaskText = taskElement.querySelector('span').textContent;
-
-    // Solicitar al usuario una nueva tarea
-    const newTaskText = prompt('Editar tarea:', currentTaskText);
-
-    // Actualizar la tarea si el usuario ingresó un nuevo texto
-    if (newTaskText !== null) {
-        taskElement.querySelector('span').textContent = newTaskText;
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+    if(tarea.value.trim()==='')
+    {
+        alert("Introduce una tarea");
     }
-}
 
-function deleteTask(button) {
-    // Obtener el elemento de la tarea actual y eliminarlo
-    const taskElement = button.parentNode;
-    taskElement.parentNode.removeChild(taskElement);
-}
+    let nuevaTarea=document.createElement('td');
+    nuevaTarea.innerHTML=`
+    <span>${tarea.value}</span>
+    <button>Editar</button>
+    <button>Eliminar</button>
+    <button id="finalizar">Finalizar</button>
+    `;
+
+    lista.appendChild(nuevaTarea);
+})
+})
